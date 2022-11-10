7e653f45af647669b572c39d46306b0f34399505
VladimirCalcina <116804951+VladimirCalcina@users.noreply.github.com>
2022-11-10 17:10:50 -0400

    Add files via upload

---
+++ TEW 200 (29-09-22)/jquery con archivo/miseventos.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    //ejercicio 9
+    $(".oculta").click(function(){
+        $(this).hide();
+    });
+        //ejercicio 10
+        $(".doble").dblclick(function(){
+            $(this).hide();
+        });
+} );
+   
